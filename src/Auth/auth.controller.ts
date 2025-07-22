1ebb3e287794f989ee53580456f4149d0159c8d8
fikrat1710 <sayilxonovfikrat0@gmail.com>
2025-07-22 18:29:36 +0500

    Fixed APP_DIR secret

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -147,4 +147,4 @@ async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: Respons
     return { message: 'Bu faqat superadminlar uchun ma‘lumot.' };
   }
 }
-// ugdegfuyerfuefbuirgy
+// ugdegfuyerfue
